4bbaa4e42a2915d48ac7b647dcb4bd386ea990a4
phil <flip.04@mail.ru>
2021-07-21 23:01:52 +0300

    lesson4

---
+++ app/src/main/java/com/example/spacemiracle/CoordinatorLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.spacemiracle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoordinatorLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coordinator_layout)
+    }
+}
